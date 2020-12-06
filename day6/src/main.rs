ab3f85ac0333ef4709b26f9249be44005c2cea3d
mahulst <michel@voorkanter.com>
2020-12-06 09:30:54 +0100

    d6.2

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -4,19 +4,30 @@ use std::fs::File;
 use std::io::Read;
 use std::ops::{BitAnd, Add};
 use std::str::FromStr;
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
 
 fn main() {
     let input = open_file("./src/input.txt");
 
     let groups = input.split("\n\n");
     let i = groups.fold(0 as usize, |count, group| {
-        group.lines().fold(HashSet::new() as HashSet<char>, |mut set, line| {
-            line.chars().for_each( |c| {
-                set.insert(c);
+        let answers = group.lines().fold(HashMap::new() as HashMap<char, usize>, |mut answers_per_group, person| {
+            person.chars().for_each( |answer| {
+                *answers_per_group.entry(answer).or_insert(0) += 1;
             });
-            set
-        }).len()  + count
+            answers_per_group
+        });
+        let people_count_in_group = group.lines().count();
+
+        let group_yes = answers.iter().fold(0 as usize, |counted_answers, (_, amount)| {
+            if *amount == people_count_in_group {
+                counted_answers + 1
+            } else {
+                counted_answers
+            }
+        });
+
+        count + group_yes
     });
 
     dbg!(i);
